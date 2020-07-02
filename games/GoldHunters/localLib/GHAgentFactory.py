9d6d636c06e496ec63a9ed9c586bd7958f186896
davinn19 <52328911+davinn19@users.noreply.github.com>
2020-07-01 19:49:28 -0700

    Revert "no message"
    This reverts commit 0142bb1a4eb970db255a05e9d39e57234afc796c.

--- games/GoldHunters/localLib/GHAgentFactory.py
+++ games/GoldHunters/localLib/GHAgentFactory.py
@@ -1,15 +1,25 @@
 from library.AgentFactory import AgentFactory
+<<<<<<< HEAD
 from GoldHunterAgent import GoldHunterAgent
+=======
+from games.GoldHunters.localLib.GoldHunterAgent import GoldHunterAgent
+>>>>>>> 6ae9b57c18ef5ec39a1bbbad7edd77f33d9bc6cc
 
 GHAgentConfig = {
 
     'diggerEfficiency': 0.8,
     'diggerDiggingRate': 10,
+<<<<<<< HEAD
     'diggerStrength': 5,
-    'robberEfficiency': 0.2,
+    'robberEfficiency': 0.4,
     'robberDiggingRate': 5,
     'robberStrength': 10
-    
+
+=======
+    'robberEfficiency': 0.2,
+    'robberDiggingRate': 5,
+>>>>>>> 6ae9b57c18ef5ec39a1bbbad7edd77f33d9bc6cc
+
 }
 
 class GHAgentFactory(AgentFactory):
@@ -22,9 +32,14 @@ class GHAgentFactory(AgentFactory):
     
     def create(self, type, id, efficiency, diggingRate, strength):
         agent = GoldHunterAgent(type = type, id = id)
+<<<<<<< HEAD
+        agent.setToOtherProperties('efficiency', efficiency)
+        agent.setToOtherProperties('diggingRate', diggingRate)
+        agent.setToOtherProperties('strength', strength)
+=======
         agent.setToOtherProperties('efficiency', efficiency) # TODO replace with GoldHunterAgent method
-        agent.setToOtherProperties('diggingRate', diggingRate)# TODO replace with GoldHunterAgent method
-        agent.setToOtherProperties('strength', strength)# TODO replace with GoldHunterAgent method
+        agent.setToOtherProperties('diggingRate', diggingRate) # TODO replace with GoldHunterAgent method
+>>>>>>> 6ae9b57c18ef5ec39a1bbbad7edd77f33d9bc6cc
         return agent
 
     

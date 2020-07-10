b5c7fefa484f7b6dd2d10adbae0e06bb8b0aebbc
Annesh Ghosh Dastidar <adastidar81@gmail.com>
2020-07-10 11:11:13 -0700

    Update GHTraitFactory.py
    Added Gold Hunter Trait Factory

---
+++ games/GoldHunters/localLib/GHTraitFactory.py
@@ -0,0 +1,30 @@
+from library.TraitFactory import TraitFactory
+
+class GHTraitFactory(TraitFactory):
+
+    def __init__(self):
+        self.randomTraitLastId = 0
+
+        self.defaultActions = [Action('MoveUp'), Action('MoveDown'), Action('MoveLeft'), Action('MoveRight'), Action('MoveUpLeft') , Action('MoveUpRight'), Action('MoveDownLeft'), Action('MoveDownRight')]
+
+    def createRandom(self):
+
+        self.randomTraitLastId += 1
+        name = 'trait-' + str(self.randomTraitLastId)
+
+        actions = self.defaultActions
+
+        actionFactory = ActionFactory()
+
+        numberOfPowers = random.randint(1, 5)
+        offensiveActions = []
+        defensiveActions = []
+
+        for _ in range(numberOfPowers):
+            power = ActionFactory.createRandomPower()
+            if power.powerType.name.find('_DEF') != -1:
+                offensiveActions.append(power)
+            else:
+                defensiveActions.append(power)
+
+        return Trait(name, actions, offensiveActions, defensiveActions)
